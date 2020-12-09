85f56d9b46ee66837da3744d463217d3d9e33b62
ernestas.bak199@go.kauko.lt <ernestas.bak199@go.kauko.lt>
2020-12-09 21:55:05 +0200

    Antras Commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pirma užduotis";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Skaičiuoti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Įveskite skaičių:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Atsakymas:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(101, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "-";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.Location = new System.Drawing.Point(104, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 24);
+            this.textBox1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(341, 258);
+            this.ClientSize = new System.Drawing.Size(399, 374);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Git Užduotys";
+            this.TransparencyKey = System.Drawing.Color.White;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+/*
+//  1. Sukurti programą kurioje naudotojas galės įvesti skaičius
+//     į vieną textbox elementą ir nuspausti mygtuką „skaičiuoti“.
+//
+//    a: nuspaudus mygtuką skaičius turi būti nuskaitomas ir 
+//       supildomas į globalų [10-ties]
+//       indeksų masyvą.
+//
+//    b: jei masyve visi skaičiai supildyti atlikti sumavimo
+//       veiksmą.
+//
+//    c: jei masyve dar nėra 10ties elementų pridėti ką
+//       tik įvestą ir parodyti label ar MessageBox elementą
+//       vartotojui, kad įvestu papildomus skaičius.
+//
+
+//  2. Papildyti pirmąją programą button ir textbox elementu.
+//     Programa turės gebėti nupiešti eglutę į richtextbox elementą:
+//     kaip pvz. jei vartotojas į laukelį įves skaičių 4 
+//     rezultatas kurį turės pateikti programa bus :
+//           *
+//          * *
+//         * * *
+//        * * * *
+//     Atlikus visas užduotis į moodle užduoties laukelį įkelti
+//     tekstinį dokumentą su nuorodą į jūsų projekto git
+//     repositoriją.
+*/
+
 
 namespace GitUzduotys
 {
@@ -16,5 +45,57 @@ namespace GitUzduotys
         {
             InitializeComponent();
         }
+
+        const int n = 10;
+
+        int position = -1;
+        double number;
+
+        double[] array = new double[n];
+        double sum = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (double.TryParse(textBox1.Text, out number))
+            {
+                // Jeigu įvestis yra skaičius
+                position += 1;
+                number = double.Parse(textBox1.Text);
+
+                label4.Text = "";
+                textBox1.Text = "";
+
+                if (position == n)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        sum += array[i];   
+                    }
+                    position = -1;
+                    label4.Text += "Suma -> " + sum.ToString();
+                    sum = 0;
+                }
+                else
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (i == position)
+                        {
+                            array[i] = number;
+                        }
+                        
+                    }
+                    label4.Text = "Įvedėte " + number + " (indeksas: " + position + ")" + " į masyvą, liko įvesti dar " + (n - (position));
+                }
+                
+            }
+            else
+            {
+                // Jeigu įvestis nėra skaičius
+                MessageBox.Show("Įvedimas turi būti skaičius!");
+                textBox1.Text = "";
+            }
+        }
     }
-}
+ }
